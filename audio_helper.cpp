3786258974e5cb5eff46df2c0ea3a287292d540e
emmett9001 <emmett.butler321@gmail.com>
2012-12-19 11:49:50 -0500

    whitespace errors, re-fix packet-marking bug reintroduced by be67c7817284a0fbac84762b4cf867b0c5b5c20a

--- audio_helper.cpp
+++ audio_helper.cpp
@@ -7,35 +7,35 @@ extern Packet *sharedBuffer;
 
 //initialize PA; return a PaStreamParameters for use with startAudio()
 PaStreamParameters getOutputParams(){
-	PaStreamParameters outputParams;
-	PaError error;
+    PaStreamParameters outputParams;
+    PaError error;
 
     Pa_Initialize();
     outputParams.device = Pa_GetDefaultOutputDevice();
-	outputParams.channelCount = OUT_CHANNELS;
-	outputParams.sampleFormat = paFloat32;
-	outputParams.suggestedLatency = Pa_GetDeviceInfo(outputParams.device)->defaultLowOutputLatency;
-	outputParams.hostApiSpecificStreamInfo = NULL;
+    outputParams.channelCount = OUT_CHANNELS;
+    outputParams.sampleFormat = paFloat32;
+    outputParams.suggestedLatency = Pa_GetDeviceInfo(outputParams.device)->defaultLowOutputLatency;
+    outputParams.hostApiSpecificStreamInfo = NULL;
 
-	/*SF_Container infile;
+    /*SF_Container infile;
 
-	infile.file = sf_open(filename, SFM_READ, &infile.info);
-	if (!(infile.file)){
-		printf("ERROR: could not open file\n");
-		return 0;
-	}*/
+    infile.file = sf_open(filename, SFM_READ, &infile.info);
+    if (!(infile.file)){
+        printf("ERROR: could not open file\n");
+        return 0;
+    }*/
 
-	return outputParams;
+    return outputParams;
 }
 
-static int paCallback( const void *inputBuffer, 
+static int paCallback( const void *inputBuffer,
     void *outputBuffer,
     unsigned long framesPerBuffer,
     const PaStreamCallbackTimeInfo* timeInfo,
-    PaStreamCallbackFlags statusFlags, 
+    PaStreamCallbackFlags statusFlags,
     void *userData)
 {
-	// Packet *sharedBuffer = (Packet*) userData;
+    // Packet *sharedBuffer = (Packet*) userData;
     int i, j, bufferIndex;
     float *out = (float*) outputBuffer, sample;
     static int order = 0;
@@ -48,48 +48,47 @@ static int paCallback( const void *inputBuffer,
         else if (i>=BUFFER_SIZE) return paContinue;
     }
     //set free to false so packet won't be overwritten
-    sharedBuffer[bufferIndex].free = false;
     //set and increment order
     sharedBuffer[bufferIndex].order = order;
     order++;
 
     //fill buffer with shit
     for (i=0; i<framesPerBuffer; i++){
-		for (j=0; j<PAC_CHANNELS; j++){
-			sample = ((rand() % 100) - 50) * .02;
-			out[2*i + j] = sample;
-			sharedBuffer[bufferIndex].frames[i][j] = sample;
-		}
-	}
-    sharedBuffer[bufferIndex].free = true;
+        for (j=0; j<PAC_CHANNELS; j++){
+            sample = ((rand() % 100) - 50) * .02;
+            out[2*i + j] = sample;
+            sharedBuffer[bufferIndex].frames[i][j] = sample;
+        }
+    }
+    sharedBuffer[bufferIndex].free = false;
 
     return paContinue;
 }
 
 //return true if no error; else print error msg and return false
 bool printError(PaError error, string msg){
-	if (error == paNoError) return true;
-	else cout << msg << Pa_GetErrorText(error) << endl;
-	return false;
+    if (error == paNoError) return true;
+    else cout << msg << Pa_GetErrorText(error) << endl;
+    return false;
 }
 
 //open and start the audio stream - takes stream, callback function, and userdata
 bool startAudio(PaStream *stream, void *userData){
 
-	PaStreamParameters outputParams = getOutputParams();
-	PaError error;
+    PaStreamParameters outputParams = getOutputParams();
+    PaError error;
 
-	error = Pa_OpenStream(&stream, NULL, &outputParams, SAMPLE_RATE, BUFFER, paNoFlag, paCallback, &userData);
-	if (! printError(error, "PortAudio error - open stream: ")) return false;
-	
-	error = Pa_StartStream(stream);
-	if (! printError(error, "PortAudio error - start stream: ")) return false;
+    error = Pa_OpenStream(&stream, NULL, &outputParams, SAMPLE_RATE, BUFFER, paNoFlag, paCallback, &userData);
+    if (! printError(error, "PortAudio error - open stream: ")) return false;
 
-	return true;
+    error = Pa_StartStream(stream);
+    if (! printError(error, "PortAudio error - start stream: ")) return false;
+
+    return true;
 }
 
 void endAudio(PaStream *stream){
-	Pa_StopStream(stream);
-	Pa_CloseStream(stream);
-	Pa_Terminate();
+    Pa_StopStream(stream);
+    Pa_CloseStream(stream);
+    Pa_Terminate();
 }
