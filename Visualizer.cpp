048c891c0023f3a6a347d7ce657bea7bbd6015b0
emmett9001 <emmett.butler321@gmail.com>
2012-12-24 17:38:15 -0500

    some args processing refactors

--- Visualizer.cpp
+++ Visualizer.cpp
@@ -19,12 +19,70 @@ bool mouseRotate = false;
 bool autoRotate = false;
 bool expTranslate = false;
 bool multiMacro = false;
+const char *windowName = "";
+const char *shapeName = "";
 extern bool finished;
+int argsMap[7];
 
 void printUsage(const char *name){
     // TODO - add a -f option for file input, default to line in
     printf("Usage: %s <soundfile> [-w hamming|hann|cosine] [-c] [-s circle|line|wave] [-t] [-r auto|mouse]\n", name);
     exit(1);
+}
+
+int findInString(char needle, char *haystack, int haystackLen){
+    for(int i = 0; i < haystackLen; i++){
+        if(haystack[i] == needle){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void processArgs(int num, char *strings[], int *map){
+    int curLength = 0;
+    // TODO - proper commandline flag checking
+    // eg allow either "-c -t -m -w hann" or "-ctm -w hann"
+    // TODO - condense this mess into bool* processArgs()
+    for(int i = 0; i < num; i++){
+        if(i + 1 > num) break;
+        curLength = strlen(strings[i]);
+        if(strings[i][0] == '-'){
+            // only allow clustered args in the first position
+            if(curLength > 2 && i > 2){
+                printUsage(strings[0]);
+            }
+            else {
+                if(findInString('c', strings[i], strlen(strings[i])) > 0){
+                    flashColors = true;
+                }
+                if(findInString('t', strings[i], strlen(strings[i])) > 0){
+                    expTranslate = true;
+                }
+                if(findInString('m', strings[i], strlen(strings[i])) > 0){
+                    multiMacro = true;
+                }
+                // these must be at the end of a cluster
+                if(findInString('w', strings[i], strlen(strings[i])) == curLength - 1){
+                    windowName = strings[i+1];
+                }
+                if(findInString('s', strings[i], strlen(strings[i])) == curLength - 1){
+                    shapeName = strings[i+1];
+                }
+            }
+        }
+
+        if(strcasecmp("-r", strings[i]) == 0){
+            if(i + 1 == num){
+                printUsage(strings[0]);
+            } else if(strcasecmp(strings[i + 1], "mouse") == 0){
+                mouseRotate = true;
+            } else if(strcasecmp(strings[i + 1], "auto") == 0){
+                autoRotate = true;
+            }
+        }
+    }
+    // return an array of bools
 }
 
 int main(int argc, char *argv[]){
@@ -35,45 +93,7 @@ int main(int argc, char *argv[]){
 
     sharedBuffer = (Packet *)malloc(sizeof(Packet) * BUFFER_SIZE);
 
-    const char *windowName = "";
-    const char *shapeName = "";
-    // TODO - proper commandline flag checking
-    // eg allow either "-c -t -m -w hann" or "-ctm -w hann"
-    // TODO - condense this mess into bool* processArgs()
-    for(int i = 0; i < argc; i++){
-        if(i + 1 > argc) break;
-        if(strcasecmp("-c", argv[i]) == 0){
-            flashColors = true;
-        }
-        else if(strcasecmp("-r", argv[i]) == 0){
-            if(i + 1 == argc){
-                printUsage(argv[0]);
-            } else if(strcasecmp(argv[i + 1], "mouse") == 0){
-                mouseRotate = true;
-            } else if(strcasecmp(argv[i + 1], "auto") == 0){
-                autoRotate = true;
-            }
-
-        }
-        else if(strcasecmp("-w", argv[i]) == 0){
-            if(i + 1 == argc){
-                printUsage(argv[0]);
-            }
-            windowName = argv[i+1];
-        }
-        else if(strcasecmp("-s", argv[i]) == 0){
-            if(i + 1 == argc){
-                printUsage(argv[0]);
-            }
-            shapeName = argv[i + 1];
-        }
-        else if(strcasecmp("-t", argv[i]) == 0){
-            expTranslate = true;
-        }
-        else if(strcasecmp("-m", argv[i]) == 0){
-            multiMacro = true;
-        }
-    }
+    processArgs(argc, argv, argsMap);
 
     if (!startAudio(stream, argv[1], windowName)){
         exit(1);
